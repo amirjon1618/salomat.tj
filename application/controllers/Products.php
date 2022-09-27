87895300366bcf4649acbd9bff8e5d381783b03b
Zaur-cmd <76215568+Zaur-cmd@users.noreply.github.com>
2022-09-27 11:00:04 +0500

    Added fucking char

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -152,7 +152,7 @@ class Products extends REST_Controller {
         for ($i = 0; $i < sizeof($categories); $i++) {
             if ($categories[$i]['category_in_main'] == 1 && $j < 3) {
                 $array[$j]['categ'] = $categories[$i];
-                $array[$j]['categ_slider'] = $this->slider->get_by_slider_category($categories[$i]['id']);
+                $array[$j]['categ_slider'] = $this->slider->get_by_slider_category($categories[$i]['id'],2);
                 $array[$j]['categ_prods'] = $this->product->get_prods_in_categ($categories[$i]['id'],$user_id?:0);
                 $j++;
             }
